cf3365d6ab32121d6bb8369184d8e105a9a0a17b
Tyrone Trevorrow <tyrone@sudeium.com>
2015-05-20 09:14:47 +1000

    Initial Commit

---
+++ RubyGens/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RubyGens
+//
+//  Created by Tyrone Trevorrow on 20/05/2015.
+//  Copyright (c) 2015 Sudeium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
