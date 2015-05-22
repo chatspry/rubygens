a25134cc16ee02e3cf1ba4ae1b08fbbf59fb51a1
Tyrone Trevorrow <tyrone@sudeium.com>
2015-05-22 19:09:06 +1000

    Adds mruby build config, gem submodules

--- mruby_config.rb
+++ mruby_config.rb
@@ -1,9 +1,27 @@
+XCODE_ROOT=%x[xcode-select -print-path].strip
+SDK_ROOT=Dir["#{XCODE_ROOT}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.1*.sdk/"].sort.last
+FFI_DIR="/usr/local/opt/libffi/lib"
+FFI_INCLUDE_DIR=Dir["#{FFI_DIR}/libffi-*/include"].sort.last
+
 MRuby::Build.new do |conf|
-  toolchain :gcc
-  conf.gem :github => "mobiruby/mruby-cfunc", :checksum_hash => "c27a12d1828de0ed4e428ca8f4aad26e1f85369b" do |g|
-    
+  toolchain :clang
+  conf.gem 'External/mruby-cfunc' do |g|
+    g.cc.include_paths << FFI_INCLUDE_DIR
+  end
+  conf.gem 'External/mruby-cocoa' do |g|
+    g.cc.include_paths << FFI_INCLUDE_DIR
+    g.objc.include_paths << FFI_INCLUDE_DIR
   end
   conf.gem :github => "pbosetti/mruby-merb", :checksum_hash => "3a7ae850a5986369612de282e5748c3528d23d9c"
-  conf.gem :github => "mobiruby/mruby-cocoa", :checksum_hash => "baa8c639f77d0b33fe6351046e20fd02535fd2ef"
   conf.gembox 'default'
+
+  [conf.cc, conf.objc].each do |cc|
+    cc.command = 'xcrun'
+    cc.flags = %W(-sdk macosx clang -isysroot #{SDK_ROOT} -O3 -Wall -Werror-implicit-function-declaration)
+  end
+
+  conf.linker do |l|
+    l.command = 'xcrun'
+    l.flags = %W(-sdk macosx clang -isysroot #{SDK_ROOT} -L#{FFI_DIR})
+  end
 end
